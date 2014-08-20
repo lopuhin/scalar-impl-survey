5871d372f389023ecedbc6d5c30879379166e54b
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2014-08-20 18:09:29 +0200

    item page layout

--- survey/urls.py
+++ survey/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from survey.views import IndexView
+from survey.views import IndexView, ItemView
 
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^survey/$', ItemView.as_view(), name='survey'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- survey/views.py
+++ survey/views.py
@@ -1,9 +1,31 @@
-from django.views.generic import TemplateView
+import random
+
+from django.shortcuts import render
+from django.views.generic import TemplateView, View
+
+from survey.models import Item, ItemSet, Filler
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
 
-class QView(TemplateView):
-    template_name = 'question.html'
+class ItemView(View):
+    template_name = 'item.html'
+
+    def get(self, request):
+        item_set = random.choice(list(ItemSet.objects.all()))
+        items = list(Item.objects\
+                .filter(item_set=item_set)\
+                .select_related('description', 'image'))
+        fillers = list(Filler.objects\
+                .select_related('description', 'image'))
+        all_items = items + fillers
+        random.shuffle(all_items)
+        first_item = all_items[0]
+        return render(request, self.template_name, {
+            'item': first_item,
+            })
+
+
+
