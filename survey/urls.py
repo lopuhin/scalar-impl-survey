b16330f46033bd72a046d3a9af9d9843f80f07d8
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2014-08-20 16:26:52 +0200

    initial page

--- survey/urls.py
+++ survey/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'survey.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+from survey.views import IndexView
 
+
+urlpatterns = patterns('',
+    url(r'^$', IndexView.as_view(), name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
