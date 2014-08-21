64d601c68a0c789a8f337afc8e388468d8fa938e
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2014-08-21 22:53:46 +0200

    open result page

--- survey/urls.py
+++ survey/urls.py
@@ -9,8 +9,8 @@ from survey.views import IndexView, ItemView, ResultView, RawResultView
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^survey/$', ItemView.as_view(), name='survey'),
-    url(r'^result/$', login_required(ResultView.as_view()), name='result'),
-    url(r'^result/raw/$', login_required(RawResultView.as_view()),
+    url(r'^result/$', ResultView.as_view(), name='result'),
+    url(r'^result/raw/$', RawResultView.as_view(),
         name='raw_result'),
     url(r'^admin/', include(admin.site.urls)),
 )
