9015b4f111bf1a44a6a240fc9975efa4f999ff91
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2014-08-21 10:04:51 +0200

    login is required

--- survey/urls.py
+++ survey/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required
 from django.contrib import admin
 admin.autodiscover()
 
@@ -8,7 +9,8 @@ from survey.views import IndexView, ItemView, ResultView, RawResultView
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^survey/$', ItemView.as_view(), name='survey'),
-    url(r'^result/$', ResultView.as_view(), name='result'),
-    url(r'^result/raw/$', RawResultView.as_view(), name='raw_result'),
+    url(r'^result/$', login_required(ResultView.as_view()), name='result'),
+    url(r'^result/raw/$', login_required(RawResultView.as_view()),
+        name='raw_result'),
     url(r'^admin/', include(admin.site.urls)),
 )
