3665af5b374e9853285b710e6e7129f905b1116f
Konstantin Lopuhin <kostia.lopuhin@gmail.com>
2014-08-22 18:10:03 +0200

    use boot to get sd, prettier plot with ggplot2

--- process/si_process.r
+++ process/si_process.r
@@ -1,4 +1,45 @@
-d = read.csv('si_raw.csv')
-dTrue = d[d$answer == 1, ]
-tPersent = table(dTrue$image, dTrue$description) / table(d$image, d$description)
-barplot(tPersent, beside=TRUE, legend.text=TRUE)
+library(boot)
+library(ggplot2)
+
+## clean up environment
+rm(list=ls())
+
+
+getTruePersent = function (d) {
+    dTrue = d[d$answer == 1, ] # true answers
+    tTrue = table(dTrue$image, dTrue$description)
+    tAll = table(d$image, d$description)
+    ## persent of true answers
+    return(tTrue / tAll)
+}
+
+getTruePersentBootstrap = function (data, indices) {
+    return(getTruePersent(data[indices,]))
+}
+
+
+full_data = read.csv('si_raw.csv')
+
+bs = boot(full_data, getTruePersentBootstrap, R=100)
+
+tPersent = getTruePersent(full_data)
+dPersent = as.data.frame(tPersent)
+colnames(dPersent) = c('image', 'description', 'answer')
+
+dPersent$sd = apply(bs$t, 2, sd)
+
+print(ggplot(
+    dPersent,
+    aes(x=description, fill=image)) +
+    geom_bar(
+        aes(y=answer),
+        position=position_dodge(width=0.9),
+        size=0.2,
+        colour='black',
+        stat='identity') +
+    geom_errorbar(
+        aes(ymin=answer-sd, ymax=answer+sd),
+        position=position_dodge(width=0.9),
+        size=0.2,
+        width=0.2)
+      )
